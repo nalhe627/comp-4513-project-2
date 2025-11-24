5ac09b10d17a35c75799cbb80c693d00ebf60577
nalhe627 <nalhe627@mtroyal.ca>
2025-11-24 12:30:03 -0700

    Add Navigation component that will hold links to the different views

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Navigation = (props) => {
+    
+    return (
+        <nav>
+            {/* <Link to="">Home</Link> */}
+            {/* <Link to=""></Link>
+            <Link to=""></Link> */}
+        </nav>
+    );
+}
+
+export default Navigation;
