d4669c10669460a60bf8121815e3f17422edacc3
nalhe627 <nalhe627@mtroyal.ca>
2025-11-25 11:39:26 -0700

    Change copyright text to include our names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = (props) => {
     return (
-        <footer className="flex flex-col w-full bg-gray-100 flex min-h-25 justify-center items-center py-4">
+        <footer className="flex flex-col w-full bg-gray-100 flex min-h-25 justify-center items-center py-4 gap-1">
             <a href="https://github.com/nalhe627/comp-4513-project-2/tree/main" target="_blank">
                 <FontAwesomeIcon icon={faGithub} size="lg" />
             </a>
-            <p className="text-gray-500/80">Copyright &copy;</p>
+            <p className="text-gray-500/80">&copy; 2025 Norris Al-Hejji & Justin Nunez</p>
         </footer>
     );
 };
