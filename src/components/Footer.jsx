91b8f3bb132106c905bc42e721b913006d13021e
nalhe627 <nalhe627@mtroyal.ca>
2025-11-25 11:36:27 -0700

    Add Github icon linked to this repository

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 const Footer = (props) => {
     return (
         <footer className="flex flex-col w-full bg-gray-100 flex min-h-25 justify-center items-center py-4">
-            <p></p>
+            <a href="https://github.com/nalhe627/comp-4513-project-2/tree/main" target="_blank">
+                <FontAwesomeIcon icon={faGithub} size="lg" />
+            </a>
             <p className="text-gray-500/80">Copyright &copy;</p>
         </footer>
     );
