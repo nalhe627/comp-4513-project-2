9fcf1f551a73051f48b3324af1d6e4ba30adc03d
nalhe627 <nalhe627@mtroyal.ca>
2025-11-27 12:55:23 -0700

    Add components for the gender filter and logic for changing the gender

--- src/views/Browse.jsx
+++ src/views/Browse.jsx
@@ -1,17 +1,58 @@
 import { useState, useContext, useEffect } from "react";
+import { Accordion, AccordionItem } from "@heroui/accordion";
+import { CheckboxGroup, Checkbox } from "@heroui/checkbox";
+import { Radio, RadioGroup } from "@heroui/radio";
 import { CartContext } from "../components/CartContext";
 
 const Browse = ({ gender, category }) => {
     const { cart, setCart } = useContext(CartContext);
-    const { loading, setLoading } = useState(true);
+    const [loading, setLoading] = useState(true);
+    // const [currGender, setCurrGender] = useState("");
 
     // Not sure if filters should be an array or object (using object for now)
-    const { filters, setFilters } = useState({gender, category});
+    const [filters, setFilters] = useState({gender, category});
+
+    /**
+     * Changes the gender property in the filter state.
+     * 
+     * For some reason it shows a warning in the console when changing this state,
+     * 
+     * @param {string} selectedGender the gender to change to in the filter state.
+     */
+    const changeGender = (selectedGender) => {
+        // setCurrGender(selectedGender);
+        setFilters({ 
+            ...filters,
+            gender: selectedGender
+        });
+    };
 
     return (
         <section className="flex border p-5 m-5 flex-grow rounded-lg gap-4">
-            <div className="border basis-1/5 p-5 rounded-lg">Filters</div>
-            <div className="border basis-4/5 p-5 rounded-lg">Products</div>
+            <div className="border basis-1/5 p-5 rounded-lg">
+                <p className="font-bold text-xl mb-5">Filters</p>
+                <Accordion selectionMode="multiple" itemClasses={{ title: "font-semibold" }}>
+                    <AccordionItem key={1} title="Gender">
+                        <RadioGroup color="default" className="pb-3" value={filters.gender} onValueChange={changeGender}>
+                            <Radio value="male">Male</Radio>
+                            <Radio value="female">Female</Radio>
+                        </RadioGroup>
+                        Selected {filters.gender}
+                    </AccordionItem>
+                    <AccordionItem key={2} title="Category">
+
+                    </AccordionItem>
+                    <AccordionItem key={3} title="Size">
+
+                    </AccordionItem>
+                    <AccordionItem key={4} title="Color">
+
+                    </AccordionItem>
+                </Accordion>
+            </div>
+            <div className="border basis-4/5 p-5 rounded-lg">
+                <p className="font-bold text-xl">Products</p>
+            </div>
         </section>
     );
 }
