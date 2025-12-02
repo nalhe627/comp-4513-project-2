a55cea39465e89e45c82b07be1a485621f5a6769
nalhe627 <nalhe627@mtroyal.ca>
2025-12-01 23:43:19 -0700

    Add ScrollShadow component to dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(accordion|button|card|checkbox|chip|divider|image|link|navbar|radio|ripple|spinner|form).js"
+    "./node_modules/@heroui/theme/dist/components/(accordion|button|card|checkbox|chip|divider|image|link|navbar|radio|scroll-shadow|ripple|spinner|form).js"
 ],
   theme: {
     extend: {},
