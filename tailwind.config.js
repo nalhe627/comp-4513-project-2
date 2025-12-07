5c1f95ba82d3dfd4723f35f9007943735999e677
nalhe627 <nalhe627@mtroyal.ca>
2025-12-06 19:01:12 -0700

    Add input component from HeroUi to package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(accordion|badge|button|card|checkbox|chip|divider|drawer|dropdown|image|link|navbar|radio|scroll-shadow|select|ripple|spinner|form|modal|menu|popover|listbox).js"
+    "./node_modules/@heroui/theme/dist/components/(accordion|badge|button|card|checkbox|chip|divider|drawer|dropdown|image|input|link|navbar|radio|scroll-shadow|select|ripple|spinner|form|modal|menu|popover|listbox).js"
 ],
   theme: {
     extend: {},
