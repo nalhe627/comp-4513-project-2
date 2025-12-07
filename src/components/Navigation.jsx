ace06f191212ca5c78df62756c45d3d3b0690d65
nalhe627 <nalhe627@mtroyal.ca>
2025-12-06 18:59:11 -0700

    Add logic to change color of the current page in the nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,47 +4,94 @@ import { Button } from "@heroui/button";
 import { Badge } from "@heroui/badge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "./CartContext";
 
-const Navigation = (props) => {
+const Navigation = () => {
     const { cart } = useContext(CartContext);
-    
+    const [currPage, setCurrPage] = useState("");
+
+    /**
+     * Changes the color of the link in the navigation bar for the current page.
+     *
+     * @param {string} route a string representing the route of the view
+     * @returns the string `"foreground"` if it's the current page, or `undefined` if not
+     */
+    const setCurrLinkColor = (route) => currPage === route ? "foreground" : undefined;
+
     return (
         <Navbar isBordered maxWidth="full">
             <NavbarContent justify="start">
                 <NavbarBrand className="">Shop</NavbarBrand>
             </NavbarContent>
             <NavbarContent justify="center">
-                <NavbarItem isActive>
-                    <Link color="foreground" isBlock href="/">Home</Link>
+                <NavbarItem isActive={currPage === "/"}>
+                    <Link
+                        color={setCurrLinkColor("/")}
+                        isBlock
+                        href="/"
+                        onPress={() => setCurrPage("/")}
+                    >
+                        Home
+                    </Link>
                 </NavbarItem>
-                <NavbarItem>
-                    <Link isBlock href="/women">Women</Link>
+                <NavbarItem isActive={currPage === "/women"}>
+                    <Link
+                        color={setCurrLinkColor("/women")}
+                        isBlock
+                        href="/women"
+                        onPress={() => setCurrPage("/women")}
+                    >
+                        Women
+                    </Link>
                 </NavbarItem>
-                <NavbarItem>
-                    <Link isBlock href="/men">Men</Link>
+                <NavbarItem isActive={currPage === "/men"}>
+                    <Link
+                        color={setCurrLinkColor("/men")}
+                        isBlock
+                        href="/men"
+                        onPress={() => setCurrPage("/men")}
+                    >
+                        Men
+                    </Link>
                 </NavbarItem>
-                <NavbarItem>
-                    <Link isBlock href="/browse">Browse</Link>
+                <NavbarItem isActive={currPage === "/browse"}>
+                    <Link
+                        color={setCurrLinkColor("/browse")}
+                        isBlock
+                        href="/browse"
+                        onPress={() => setCurrPage("/browse")}
+                    >
+                        Browse
+                    </Link>
                 </NavbarItem>
                 <NavbarItem>
                     <Link isBlock href="/about">About</Link>
                 </NavbarItem>
             </NavbarContent>
             <NavbarContent justify="end">
+                <NavbarItem isActive={currPage === "/login"}>
+                    <Link
+                        color={setCurrLinkColor("/login")}
+                        isBlock
+                        href="/login"
+                        onPress={() => setCurrPage("/login")}
+                    >
+                        Admin Login
+                    </Link>
+                </NavbarItem>
                 {/* Using /cart for now for the shopping cart route */}
-                <Button 
-                    as={Link} 
-                    variant="ghost" 
-                    href="/cart" 
-                    isIconOnly 
+                <Button
+                    as={Link}
+                    variant="ghost"
+                    href="/cart"
+                    isIconOnly
                     className="border-transparent overflow-visible"
                 >
-                    <Badge 
-                        color="danger" 
-                        content={cart.length} 
-                        isInvisible={cart.length === 0} 
+                    <Badge
+                        color="danger"
+                        content={cart.length}
+                        isInvisible={cart.length === 0}
                         shape="circle"
                         showOutline={false}
                         size="sm"
@@ -55,6 +102,6 @@ const Navigation = (props) => {
             </NavbarContent>
         </Navbar>
     );
-}
+};
 
 export default Navigation;
