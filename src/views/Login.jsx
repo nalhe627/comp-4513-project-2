a06cb42a5690f62a449c62a66a56f0685fc9f61e
nalhe627 <nalhe627@mtroyal.ca>
2025-12-08 10:36:20 -0700

    Add tooltip to registration button in login view

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -4,11 +4,19 @@ import { LoginContext } from "../components/LoginContext";
 import { Form } from "@heroui/form";
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
+import { Tooltip } from "@heroui/tooltip";
 
 const Login = () => {
     const { cart, setCart } = useContext(CartContext);
     const { loggedIn, setLoggedIn } = useContext(LoginContext);
 
+    /**
+     * Attempts to sign the user in.
+     * 
+     * Since auth hasn't been implemented, this merely changes the Login context.
+     * 
+     * @param {Event} e Event that occurs when submitting a form
+     */
     const signIn = (e) => {
         e.preventDefault();
         setLoggedIn(true);
@@ -21,10 +29,16 @@ const Login = () => {
             <Input label="Password" isRequired type="password" />
             <div className="flex flex-row gap-5 w-full justify-center">
                 <Button color="primary" type="submit">Log in</Button>
-                <Button>Register</Button>
+                <Tooltip 
+                    content="Registeration feature hasn't been added yet. Check back soon for this feature!" 
+                    showArrow 
+                    offset={5}
+                >
+                    <Button>Register</Button>
+                </Tooltip>
             </div>
             <p className="text-gray-800/50 text-xs">
-                Note: Authentication hasn't been implemented, so this login page is purely simulated
+                Note: Authentication hasn't been implemented, so this login page is purely simulated.
             </p>
         </Form>
     );
