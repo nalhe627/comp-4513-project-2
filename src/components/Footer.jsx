6ae227dcaaae3e30d5c26c827f92309223ea19e7
nalhe627 <nalhe627@mtroyal.ca>
2025-12-08 20:26:15 -0700

    Remove some unused variables

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-const Footer = (props) => {
+const Footer = () => {
     return (
         <footer className="flex flex-col w-full bg-gray-100 flex min-h-25 justify-center items-center py-4 gap-1">
             <a href="https://github.com/nalhe627/comp-4513-project-2/tree/main" target="_blank">
