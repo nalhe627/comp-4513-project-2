e78aeef83e921186f143a6facfd760c142f9aba1
nalhe627 <nalhe627@mtroyal.ca>
2025-12-08 19:39:12 -0700

    Refactor Filter to be its own component

--- src/views/Browse.jsx
+++ src/views/Browse.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
-import { Accordion, AccordionItem } from "@heroui/accordion";
-import { Checkbox, CheckboxGroup } from "@heroui/checkbox";
-import { Radio, RadioGroup } from "@heroui/radio";
 import { Chip } from "@heroui/chip";
 import { Select, SelectItem } from "@heroui/select";
 
 import ProductList from "../components/ProductList";
-import { CATEGORIES, SIZES, COLORS } from "../constants/filters";
-
+import Filter from "../components/Filter";
 
 const Browse = ({ products, gender, categories, changeProduct }) => {
     const [loading, setLoading] = useState(true);
@@ -194,179 +190,19 @@ const Browse = ({ products, gender, categories, changeProduct }) => {
         }
     };
 
-    /**
-     * Changes the gender property in the filter state.
-     *
-     * *For some reason it shows a warning in the console when changing this state,
-     * But when you use a separate state for gender, it doesn't show.*
-     *
-     * @param {string} selectedGender the gender to change to in the filter state.
-     */
-    const changeGender = (selectedGender) => {
-        setSelectedGender(selectedGender);
-        const currFilters = { ...filters, gender: selectedGender };
-
-        const noGenderArr = filterArr.filter((f) => f.department !== "gender");
-
-        setFilterArr([
-            ...noGenderArr, 
-            { department: "gender", value: selectedGender }
-        ]);
-        refilterProducts(currFilters);
-    };
-
-    /**
-     * Changes the categories property in the filter state.
-     * 
-     * @param {string[]} selectedCategories the categories to change in the filter state.
-     */
-    const changeCategories = (selectedCategories) => {
-        const currFilters = { ...filters, categories: selectedCategories };
-
-        const categoryArr = selectedCategories.map((category) => ({
-            department: "categories", 
-            value: category
-        }));
-        
-        const noCategoryArr = filterArr.filter((f) => f.department !== "categories");
-
-        setFilterArr([
-            ...noCategoryArr,
-            ...categoryArr
-        ]);
-        refilterProducts(currFilters);
-    }
-
-    /**
-     * Changes the sizes property in the filter state.
-     * 
-     * @param {string[]} selectedSizes the sizes to change in the filter state.
-     */
-    const changeSizes = (selectedSizes) => {
-        const currFilters = { ...filters, sizes: selectedSizes };
-        
-        const sizeArr = selectedSizes.map((size) => ({
-            department: "sizes", 
-            value: size
-        }));
-        
-        const noSizeArr = filterArr.filter((f) => f.department !== "sizes");
-
-        setFilterArr([
-            ...noSizeArr,
-            ...sizeArr
-        ]);
-        refilterProducts(currFilters);
-    }
-
-    /**
-     * Changes the color property in the filter state.
-     * 
-     * @param {string[]} selectedColors the colors to change in the filter state.
-     */
-    const changeColors = (selectedColors) => {
-        const currFilters = { ...filters, colors: selectedColors };
-
-        const colorArr = selectedColors.map((color) => ({
-            department: "colors", 
-            value: color
-        }));
-        
-        const noColorArr = filterArr.filter((f) => f.department !== "colors");
-
-        setFilterArr([
-            ...noColorArr,
-            ...colorArr
-        ]);
-        refilterProducts(currFilters);
-    }
-
     return (
         <section className="flex p-5 m-5 items-start flex-grow rounded-lg gap-4">
 
             {/* Filter section */}
             {/* TODO: make the fitler a separate component */}
-            <div className="bg-gray-100 basis-1/5 p-5 rounded-lg shadow-sm">
-                <p className="font-bold text-xl mb-5">Filters</p>
-                <Accordion
-                    selectionMode="multiple"
-                    itemClasses={{ title: "font-semibold" }}
-                >
-                    {/* Gender filter */}
-                    <AccordionItem key={1} title="Gender">
-                        <RadioGroup
-                            color="default"
-                            className="pb-3"
-                            value={selectedGender}
-                            onValueChange={changeGender}
-                            
-                        >
-                            <Radio value="mens">Men</Radio>
-                            <Radio value="womens">Women</Radio>
-                        </RadioGroup>
-                    </AccordionItem>
-
-                    {/* Category filter */}
-                    <AccordionItem key={2} title="Category">
-                        <CheckboxGroup 
-                            color="default" 
-                            className="pb-3"
-                            value={filters.categories} 
-                            onValueChange={changeCategories}
-                        >
-                            {CATEGORIES.map((categ, i) => (
-                                <Checkbox key={i} value={categ}>
-                                    {categ}
-                                </Checkbox>
-                            ))}
-                        </CheckboxGroup>
-                    </AccordionItem>
-
-                    {/* Size filter */}
-                    <AccordionItem key={3} title="Size">
-                        <CheckboxGroup 
-                            color="default" 
-                            className="pb-3"
-                            value={filters.sizes} 
-                            onValueChange={changeSizes}
-                        >
-                            {SIZES.map((size, i) => (
-                                <Checkbox key={i} value={size}>
-                                    {size}
-                                </Checkbox>
-                            ))}
-                        </CheckboxGroup>
-                    </AccordionItem>
-
-                    {/* Color filter */}
-                    {/* TODO: make all color boxes with the same alignment */}
-                    <AccordionItem key={4} title="Color">
-                        <CheckboxGroup 
-                            color="default" 
-                            value={filters.colors} 
-                            onValueChange={changeColors}
-                        >
-                            {COLORS.map((color, i) => {
-                                return (
-                                    <Checkbox key={i} value={color.name}>
-                                        <div className="grid grid-cols-2 gap-6">
-                                            <p>
-                                                {color.name}
-                                            </p>
-
-                                            {/* Hardcoding the colorbox witdh for now */}
-                                            <div 
-                                                className={`${color.hex} border rounded-xs w-[24px]`}
-                                            >
-                                            </div>
-                                        </div>
-                                    </Checkbox>
-                                )
-                            })}
-                        </CheckboxGroup>
-                    </AccordionItem>
-                </Accordion>
-            </div>
+            <Filter 
+                filters={filters} 
+                filterArr={filterArr} 
+                setFilterArr={setFilterArr} 
+                selectedGender={selectedGender} 
+                setSelectedGender={setSelectedGender} 
+                refilterProducts={refilterProducts} 
+            />
 
             {/* Product section */}
             {/* TODO: put into separate component */}
